76e46d82afaaa2e854a23af6f7d9ac2138bd2861
Phil Trinh <ptrinh@me.com>
2026-09-20 12:13:58 +0800

    dotbar 0.1.0

---
+++ Casks/dotbar.rb
@@ -0,0 +1,18 @@
+cask "dotbar" do
+  version "0.1.0"
+  sha256 "0d9561221de724a32139762b4aad404c5c5e59d923f7e033c19383e53a62146b"
+
+  url "https://github.com/ptrinh/DotBar/releases/download/v#{version}/DotBar-#{version}.zip"
+  name "DotBar"
+  desc "Custom text and colored status dots for the macOS menu bar"
+  homepage "https://github.com/ptrinh/DotBar"
+
+  depends_on macos: ">= :sonoma"
+
+  app "DotBar.app"
+
+  zap trash: [
+    "~/Library/Application Support/DotBar",
+    "~/Library/Preferences/com.ptrinh.DotBar.plist",
+  ]
+end
