086df9351064bf0ff627ee5b680ecaef63aa75b8
Phil Trinh <ptrinh@me.com>
2026-09-20 12:33:45 +0800

    dotbar 0.1.2

--- Casks/dotbar.rb
+++ Casks/dotbar.rb
@@ -1,6 +1,6 @@
 cask "dotbar" do
-  version "0.1.1"
-  sha256 "34d56f561670dc1df2eb26506cc8e58285185c9aa69368ecfdb245dc36cace35"
+  version "0.1.2"
+  sha256 "26086e55315bf618af8b14f5b9ff666f6c6ee4bcf80bc1d13ad670035789089b"
 
   url "https://github.com/ptrinh/DotBar/releases/download/v#{version}/DotBar-#{version}.zip"
   name "DotBar"
